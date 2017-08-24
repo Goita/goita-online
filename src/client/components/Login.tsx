0d8c8037aa08df284cd63c635f62067a8a9a2bf9
yoskeoka <yoske95@gmail.com>
2017-08-24 23:37:40 +0900

    upgrade login

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,24 +1,23 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
+import Avatar from "material-ui/Avatar";
+import LockIcon from "material-ui/svg-icons/action/lock-outline";
+import { Card, CardTitle, CardText, CardActions } from "material-ui/Card";
 import RaisedButton from "material-ui/RaisedButton";
 import FontIcon from "material-ui/FontIcon";
+import { cyan500, pinkA200 } from "material-ui/styles/colors";
 
 import * as styles from "./Login.css";
 
-interface LoginProps extends RouteComponentProps<any> {
-    isAuthenticated: boolean;
-}
-
 interface LoginState {
     fetching: boolean;
-    loggedin: boolean;
 }
 
-export default class Login extends React.Component<LoginProps, LoginState> {
+export default class Login extends React.Component<RouteComponentProps<any>, LoginState> {
 
     constructor() {
         super();
-        this.state = { fetching: true, loggedin: false };
+        this.state = { fetching: true };
     }
 
     componentDidMount() {
@@ -33,70 +32,78 @@ export default class Login extends React.Component<LoginProps, LoginState> {
             credentials: "same-origin", // auto send cookies
         }).then((res) => {
             if (res.status === 200) {
-                this.setState({ fetching: false, loggedin: true });
+                location.href = "/";
             } else {
-                this.setState({ fetching: false, loggedin: false });
+                this.setState({ fetching: false });
             }
         });
     }
 
-    public notLoginRender() {
+    public render() {
         return (
-            <div>
-                <h1 className={styles.title} > ログイン方法を選択してください</h1>
-                <RaisedButton
-                    href="/auth/twitter"
-                    target="_blank"
-                    label="Twitterでログイン"
-                    backgroundColor="#1ab7ea"
-                    labelColor="#FFFFFF"
-                    labelStyle={{ textTransform: "none" }}
-                    className={styles.snsBtn}
-                    icon={<FontIcon className="fa fa-twitter" />}
-                />
-                <RaisedButton
-                    href="/auth/facebook"
-                    target="_blank"
-                    label="Facebookでログイン"
-                    backgroundColor="#3b5998"
-                    labelColor="#FFFFFF"
-                    labelStyle={{ textTransform: "none" }}
-                    className={styles.snsBtn}
-                    icon={<FontIcon className="fa fa-facebook" />}
-                />
-                <RaisedButton
-                    href="/auth/google"
-                    target="_blank"
-                    label="Google+でログイン"
-                    backgroundColor="#dd4b39"
-                    labelColor="#FFFFFF"
-                    labelStyle={{ textTransform: "none" }}
-                    className={styles.snsBtn}
-                    icon={<FontIcon className="fa fa-google-plus" />}
-                />
+            <div className={styles.main} style={{ backgroundColor: cyan500 }}>
+                {this.state.fetching ? this.fetching() : this.login()}
             </div >
         );
     }
 
-    public loggedInRender() {
+    private fetching(): JSX.Element {
         return (
-            <div>
-                <p>You're already logged in.</p>
-                <a href="/logout">Logout</a>
-            </div>
+            <Card>
+                <CardTitle title="ログイン" subtitle="ごいたオンライン認証画面" />
+                <CardText>
+                    ログイン状況を確認中・・・
+                </CardText>
+            </Card>
         );
     }
 
-    public render() {
-
-        if (this.state.fetching) {
-            return <div>checking Login status...</div>;
-        } else {
-            if (this.state.loggedin) {
-                return this.loggedInRender();
-            } else {
-                return this.notLoginRender();
-            }
-        }
+    private login(): JSX.Element {
+        return (
+            <Card>
+                <CardTitle title="ログイン" subtitle="ごいたオンライン認証画面" />
+                <CardText className={styles.text}>
+                    いずれかのSNSアカウントでログインしてください。
+                </CardText>
+                <CardText className={styles.text}>
+                    SNSアカウントの登録IDが他のユーザに表示されることはありませんが、ユーザ名と画像アイコンは初期状態でログインに使用したSNSアカウントのものが使用されます。この設定はログイン後に変更することが可能です。
+                </CardText>
+                <CardActions>
+                    <RaisedButton
+                        href="/auth/twitter"
+                        target="_blank"
+                        label="Twitter"
+                        backgroundColor="#1ab7ea"
+                        labelColor="#FFFFFF"
+                        labelStyle={{ textTransform: "none" }}
+                        className={styles.snsBtn}
+                        style={{ display: "block" }}
+                        icon={<FontIcon className="fa fa-twitter" />}
+                    />
+                    <RaisedButton
+                        href="/auth/facebook"
+                        target="_blank"
+                        label="Facebook"
+                        backgroundColor="#3b5998"
+                        labelColor="#FFFFFF"
+                        labelStyle={{ textTransform: "none" }}
+                        className={styles.snsBtn}
+                        style={{ display: "block" }}
+                        icon={<FontIcon className="fa fa-facebook" />}
+                    />
+                    <RaisedButton
+                        href="/auth/google"
+                        target="_blank"
+                        label="Google"
+                        backgroundColor="#dd4b39"
+                        labelColor="#FFFFFF"
+                        labelStyle={{ textTransform: "none" }}
+                        className={styles.snsBtn}
+                        style={{ display: "block" }}
+                        icon={<FontIcon className="fa fa-google" />}
+                    />
+                </CardActions>
+            </Card>
+        );
     }
 }
