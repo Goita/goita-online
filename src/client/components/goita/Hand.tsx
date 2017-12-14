480263f9028347999d99f2c1d8f2a28af90fa3ab
yoskeoka <yoske95@gmail.com>
2017-12-15 04:27:18 +0900

    tmp

--- src/client/components/goita/Hand.tsx
+++ src/client/components/goita/Hand.tsx
@@ -4,19 +4,41 @@ import * as goita from "goita-core";
 import Button from "material-ui/Button";
 import Badge from "material-ui/Badge";
 import KomaIcon from "./KomaIcon";
+import PlayPreview from "./PlayPreview";
+import Grid from "material-ui/Grid";
 
 import { withStyles, WithStyles, StyleRules } from "material-ui/styles";
 
-type classKeys = "block" | "relPos" | "absPos";
+type classKeys = "container" | "itemPreview" | "itemReset" | "itemPlay" | "itemHands";
 const styles: StyleRules<classKeys> = {
-    block: {
-        display: "block",
+    container: {
+        display: "grid",
+        gridTemplateColumns: "140px 50px 50px auto",
+        gridTemplateRows: "60px 80px",
     },
-    relPos: {
-        position: "relative",
+    itemPreview: {
+        gridColumnStart: 1,
+        gridColumnEnd: "span 1",
+        gridRowStart: 1,
+        gridRowEnd: "span 1",
     },
-    absPos: {
-        position: "absolute",
+    itemReset: {
+        gridColumnStart: 2,
+        gridColumnEnd: "span 1",
+        gridRowStart: 1,
+        gridRowEnd: "span 1",
+    },
+    itemPlay: {
+        gridColumnStart: 3,
+        gridColumnEnd: "span 1",
+        gridRowStart: 1,
+        gridRowEnd: "span 1",
+    },
+    itemHands: {
+        gridColumnStart: 1,
+        gridColumnEnd: "span 4",
+        gridRowStart: 2,
+        gridRowEnd: "span 1",
     },
 };
 
@@ -41,42 +63,36 @@ class Hand extends React.Component<Props & WithStyles<classKeys>, State> {
     public render() {
         const classes = this.props.classes;
         const list = this.removeHand(this.props.hand, this.state.block, this.state.attack).map((k, i) => (
-            <Button raised disabled={!this.props.canPlay} key={k.value + i} onClick={this.stagePlayKoma(k)}>
-                <KomaIcon koma={k} />
-            </Button>
+            // <Button raised disabled={!this.props.canPlay} key={k.value + i} onClick={this.stagePlayKoma(k)}>
+            <Grid item xs={1}>
+                <KomaIcon koma={k} onClick={this.stagePlayKoma(k)} />
+            </Grid>
+            // {/* </Button> */}
         ));
-
-        const previewKoma = (type: string, koma: goita.Koma): JSX.Element => {
-            return (
-                <Badge badgeContent={type}>
-                    <KomaIcon koma={koma} />
-                </Badge>
-            );
-        };
-
-        const blockPreview = previewKoma("受", this.state.block);
-        const attackPreview = previewKoma("攻", this.state.attack);
 
         const disabledPlay = this.props.noPreviewAttack || !this.state.attack;
 
         return (
-            <div>
-                <div className={classes.block}>
-                    {blockPreview}
-                    {attackPreview}
-                    {this.state.block && (
-                        <Button raised onClick={this.resetPlayKoma}>
-                            リセット
+            <div className={classes.container}>
+                <PlayPreview className={classes.itemPreview} block={this.state.block} attack={this.state.attack} />
+                {this.state.block && (
+                    <Button className={classes.itemReset} raised onClick={this.resetPlayKoma}>
+                        リセット
+                    </Button>
+                )}
+                {this.state.block &&
+                    this.state.attack && (
+                        <Button className={classes.itemPlay} raised disabled={disabledPlay} onClick={this.play}>
+                            打つ
                         </Button>
                     )}
-                    {this.state.block &&
-                        this.state.attack && (
-                            <Button raised disabled={disabledPlay} onClick={this.play}>
-                                打つ
-                            </Button>
-                        )}
-                </div>
-                <div className={classes.block}>{list}</div>
+                <Grid className={classes.itemHands} container spacing={8}>
+                    <Grid item xs={12}>
+                        <Grid container spacing={8}>
+                            {list}
+                        </Grid>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
--- src/client/components/goita/KomaIcon.tsx
+++ src/client/components/goita/KomaIcon.tsx
@@ -13,8 +13,10 @@ const styles: StyleRules<classKeys> = {
     },
 };
 
-interface Props {
+interface Props extends React.CSSProperties {
     koma?: goita.Koma | null;
+    onClick?: () => void;
+    className?: string;
 }
 
 interface State {}
@@ -23,20 +25,27 @@ class KomaIcon extends React.Component<Props & WithStyles<classKeys>, State> {
     public render() {
         const props = this.props;
         const classes = props.classes;
+        let ret = null;
         if (props.koma) {
-            return (
+            ret = (
                 <div className={classes.relPos}>
                     <img className={classes.absPos} src={"/images/koma.png"} />
-                    <img className={classes.absPos} src={"/images/koma" + props.koma.value + ".png"} />
+                    <img
+                        className={classes.absPos}
+                        src={"/images/koma" + props.koma.value + ".png"}
+                        onClick={this.props.onClick}
+                    />
                 </div>
             );
         } else {
-            return (
+            ret = (
                 <div className={classes.relPos}>
-                    <img className={classes.absPos} src={"/images/koma0.png"} />
+                    <img className={classes.absPos} src={"/images/koma0.png"} onClick={this.props.onClick} />
                 </div>
             );
         }
+
+        return <div className={this.props.className}>{ret}</div>;
     }
 }
 
